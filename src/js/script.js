bf3875303c1f4665ba42f511c9cdf2245c555233
Leszek <leszek.synowiec@gmail.com>
2020-01-13 22:24:44 +0100

    Add renderInMenu and initAcordionfunction

--- src/js/script.js
+++ src/js/script.js
@@ -52,7 +52,62 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product{
+    constructor(id, data){
+      const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
+      thisProduct.renderInMenu();
+      thisProduct.initAccordion();
+      console.log('new Product:', thisProduct);
+
+    }
+    renderInMenu(){
+      const thisProduct = this;
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      menuContainer.appendChild(thisProduct.element);
+    }
+    initAccordion(){
+      const thisProduct = this;
+      const button = thisProduct.element.querySelector(select.menuProduct.clickable);
+      button.addEventListener('click', function(){
+        event.preventDefault;
+        thisProduct.element.classList.toggle('active');
+        const activeProducts = document.querySelectorAll('article.active');
+        // console.log(activeProducts)
+        for(let activeProduct of activeProducts){
+          // console.log(activeProduct)
+          if(activeProduct == thisProduct.element){
+            continue;
+          }else{
+            activeProduct.classList.remove('active');}
+          }
+        }
+          )}
+      // console.log(button);
+
+        }
+
+
   const app = {
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
+    initMenu: function(){
+      const thisApp = this;
+      console.log('thisApp.data', thisApp.data);
+
+      for(let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,6 +115,9 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
